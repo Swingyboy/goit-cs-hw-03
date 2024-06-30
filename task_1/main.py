88d30540f65ab775923dd785f352ef54e8de6410
serhii <sbezuhlyi@tektelic.com>
2024-06-30 20:08:54 +0300

    add separation by tasks

--- task_1/main.py
+++ task_1/main.py
@@ -2,14 +2,139 @@ from sqlalchemy.orm import sessionmaker
 
 
 from db_engine import DBEngineFactory
-from tables import TableHandler
+from tables import DatabaseManager
 
+RED_COLOR = "\033[91m"
+WHITE_COLOR = "\033[0m"
+GREEN_COLOR = "\033[92m"
+YELLOW_COLOR = "\033[93m"
 
 POSTGRES_HOST = "localhost"
 POSTGRES_PORT = 5432
 POSTGRES_USER = "postgres"
 POSTGRES_PASSWORD = "mypassword"
 DB_NAME = "postgres"
+
+
+def print_items(items):
+    if not items:
+        print(RED_COLOR + "No records found in data base".upper() + WHITE_COLOR)
+    else:
+        for item in items:
+            print("-" * 50)
+            print(item)
+
+
+def task_1(manager):
+    """Get random user and all tasks of this user."""
+    print(GREEN_COLOR + "Get random user:".upper() + WHITE_COLOR)
+    random_user = manager.get_random_user()
+    print(random_user)
+    print("Get all tasks of this user:".upper())
+    tasks = manager.get_tasks_of_user(random_user.id)
+    print_items(tasks)
+
+
+def task_2(manager):
+    """Get status and all tasks with this status."""
+    input_status = input(YELLOW_COLOR + "Enter status: " + WHITE_COLOR)
+    status = manager.get_status_by_name(input_status)
+    print(status)
+    if not status:
+        print(RED_COLOR + "No status found".upper() + WHITE_COLOR)
+        return
+    print(GREEN_COLOR + "Get all tasks with this status:".upper() + WHITE_COLOR)
+    tasks = manager.get_tasks_with_status(status.id)
+    print_items(tasks)
+
+
+def task_3(manager):
+    """Update task status"""
+
+
+def task_4(manager):
+
+    print(GREEN_COLOR + "Get all users without tasks:".upper() + WHITE_COLOR)
+    users = manager.get_all_users_without_tasks()
+    print_items(users)
+
+
+def task_5(manager):
+    random_user = manager.get_random_user()
+    random_status = manager.get_random_status()
+    print(GREEN_COLOR + "Add a new task to ".upper() + str(random_user) + " with ".upper() + str(random_status) + WHITE_COLOR)
+    manager.add_task_to_user(random_user.id, random_status.id)
+    tasks = manager.get_tasks_of_user(random_user.id)
+    print_items(tasks)
+
+
+def task_6(manager):
+    print(GREEN_COLOR + "Get all tasks these are not done".upper() + WHITE_COLOR)
+    not_done_tasks = manager.get_tasks_not_done()
+    print(not_done_tasks)
+
+
+def task_7(manager):
+    print(GREEN_COLOR + "Get random task".upper() + WHITE_COLOR)
+    random_task = manager.get_random_task()
+    print(random_task)
+    print(GREEN_COLOR + "Delete this task".upper() + WHITE_COLOR)
+    manager.delete_task(random_task.id)
+    tasks = manager.get_all_tasks()
+    print_items(tasks)
+
+
+def task_8(manager):
+    email_pattern = input(YELLOW_COLOR + "Enter email pattern: " + WHITE_COLOR)
+    if not email_pattern:
+        email_pattern = "gmail.com"
+    print(GREEN_COLOR + "Get all users with email like: ".upper() + email_pattern + WHITE_COLOR)
+    users = manager.get_users_by_email_pattern(email_pattern)
+    print_items(users)
+
+
+def task_9(manager):
+    random_user = manager.get_random_user()
+    print(GREEN_COLOR + "Update user".upper() + str(random_user) + WHITE_COLOR)
+    new_full_name = input(YELLOW_COLOR + "Enter new full name: " + WHITE_COLOR)
+    new_email = input(YELLOW_COLOR + "Enter new email: " + WHITE_COLOR)
+    manager.update_user(random_user.id, new_full_name, new_email)
+    print(GREEN_COLOR + "User was updated" + WHITE_COLOR)
+    print(manager.get_user_by_id(random_user.id))
+
+
+def task_10(manager):
+    print(GREEN_COLOR + "Get number of tasks for each status".upper() + WHITE_COLOR)
+    tasks_number = manager.get_number_of_tasks_for_each_status()
+    for number in tasks_number:
+        print(number[0], number[1])
+
+
+def task_11(manager):
+    print(GREEN_COLOR + "Get tasks of users with email pattern".upper() + WHITE_COLOR)
+    pattern = input(YELLOW_COLOR + "Enter email pattern: " + WHITE_COLOR)
+    if not pattern:
+        pattern = "gmail.com"
+    tasks = manager.get_tasks_of_users_with_email_pattern(pattern)
+    print_items(tasks)
+
+
+def task_12(manager):
+    print(GREEN_COLOR + "Get all tasks without description".upper() + WHITE_COLOR)
+    tasks = manager.get_tasks_without_description()
+    print_items(tasks)
+
+
+def task_13(manager, status="In Progress"):
+    print(GREEN_COLOR + "Get users with task with status: ".upper() + status + WHITE_COLOR)
+    users = manager.get_users_by_task_status(status)
+    print_items(users)
+
+
+def task_14(manager):
+    print(GREEN_COLOR + "Get all users with task counts".upper() + WHITE_COLOR)
+    users = manager.get_users_with_task_counts()
+    print_items(users)
 
 
 def main():
@@ -22,34 +147,22 @@ def main():
         db=DB_NAME,
         echo=False
     )
-    tb_handler = TableHandler(db_engine)
-    tb_handler.setup(5, ["New", "In Progress", "Done"], 4)
-
-    print("Get random user:".upper())
-    user = tb_handler.get_random_user()
-    print(user)
-    print("Get all tasks of this user:".upper())
-    tasks = tb_handler.get_tasks_of_user(user.id)
-    for task in tasks:
-        print("-" * 50)
-        print(task)
-
-    print("Get random status:".upper())
-    status = tb_handler.get_random_status()
-    print(status)
-    print("Get all tasks with this status:".upper())
-    tasks = tb_handler.get_tasks_with_status(status.id)
-    for task in tasks:
-        print("-" * 50)
-        print(task)
-
-    print("Get all users without tasks:".upper())
-    users = tb_handler.get_all_users_without_tasks()
-    if not users:
-        print("All users have tasks")
-    else:
-        for user in users:
-            print(user)
+    database_manager = DatabaseManager(db_engine)
+    database_manager.setup(5, ["New", "In Progress", "Done"], 4)
+    task_1(database_manager)
+    task_2(database_manager)
+    task_3(database_manager)
+    task_4(database_manager)
+    task_5(database_manager)
+    task_6(database_manager)
+    task_7(database_manager)
+    task_8(database_manager)
+    task_9(database_manager)
+    task_10(database_manager)
+    task_11(database_manager)
+    task_12(database_manager)
+    task_13(database_manager)
+    task_14(database_manager)
 
 
 if __name__ == "__main__":
--- task_1/tables.py
+++ task_1/tables.py
@@ -1,8 +1,8 @@
 from faker import Faker
 from random import choice
-from sqlalchemy import Column, Integer, ForeignKey, String, Sequence, Text
+from sqlalchemy import Column, Integer, ForeignKey, String, Sequence, Text, func
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import aliased, relationship, sessionmaker
 
 
 Base = declarative_base()
@@ -46,7 +46,7 @@ class Tasks(Base):
         return f"Task ({self.title}, {self.description}, {self.status_id})"
 
 
-class TableHandler:
+class DatabaseManager:
     def __init__(self, engine):
         self.faker = Faker()
         self.engine = engine
@@ -71,9 +71,50 @@ class TableHandler:
     def get_all_users_without_tasks(self):
         return self.session.query(Users).filter(Users.tasks == None).all()
 
+    def get_users_by_email_pattern(self, pattern: str):
+        return self.session.query(Users).filter(Users.email.like(f"%{pattern}%")).all()
+
+    def get_users_by_task_status(self, status_name: str):
+        return self.session.query(
+            Users.id,
+            Users.full_name,
+            Users.email,
+            Tasks.title,
+            Tasks.description,
+            Status.name.label('status')
+        ).join(Tasks, Users.id == Tasks.user_id
+               ).join(Status, Tasks.status_id == Status.id
+                      ).filter(Status.name == status_name
+                               ).all()
+
+    def get_users_with_task_counts(self):
+        result = self.session.query(
+            Users.id,
+            Users.full_name,
+            Users.email,
+            func.count(Tasks.id).label('task_count')
+        ).outerjoin(Tasks, Users.id == Tasks.user_id
+                    ).group_by(Users.id, Users.full_name, Users.email
+                               ).all()
+
+        return result
+
+    def get_user_by_id(self, user_id: int):
+        return self.session.query(Users).filter(Users.id == user_id).first()
+
     def get_random_user(self):
         users = self.get_all_users()
         return choice(users)
+
+    def update_user(self, user_id: int, new_full_name: str = None, new_email: str = None):
+        user = self.session.query(Users).filter(Users.id == user_id).first()
+        if not new_full_name or new_full_name == "":
+            new_full_name = self.faker.name()
+        if not new_email or new_email == "":
+            new_email = self.faker.email()
+        user.full_name = new_full_name
+        user.email = new_email
+        self.session.commit()
 
     def create_statuses(self, status_list: list):
         for st_name in status_list:
@@ -89,8 +130,19 @@ class TableHandler:
         statuses = self.get_all_statuses()
         return choice(statuses)
 
+    def get_status_by_name(self, status_name: str):
+        return self.session.query(Status).filter(Status.name == status_name).first()
+
     def delete_all_statuses(self):
         self.session.query(Status).delete()
+        self.session.commit()
+
+    def add_task_to_user(self, user_id: int, status_id: int):
+        title = f"Task for User {user_id}"
+        description = f"Description for Task for User {user_id}. {self.faker.text()}"
+        statuses = self.get_all_statuses()
+        task = Tasks(title=title, description=description, status_id=status_id, user_id=user_id)
+        self.session.add(task)
         self.session.commit()
 
     def create_tasks(self, task_number: int):
@@ -108,14 +160,48 @@ class TableHandler:
     def get_all_tasks(self):
         return self.session.query(Tasks).all()
 
+    def get_tasks_without_description(self):
+        return self.session.query(Tasks).filter(Tasks.description == None).all()
+
+    def get_tasks_not_done(self):
+        done_status = self.session.query(Status).filter(Status.name == "Done").first()
+        return self.session.query(Tasks).filter(Tasks.status_id != done_status.id).all()
+
     def get_tasks_of_user(self, user_id: int):
         return self.session.query(Tasks).filter(Tasks.user_id == user_id).all()
 
     def get_tasks_with_status(self, status_id: int):
         return self.session.query(Tasks).filter(Tasks.status_id == status_id).all()
 
+    def get_tasks_of_users_with_email_pattern(self, pattern: str):
+        result = self.session.query(
+            Users.id,
+            Users.full_name,
+            Users.email,
+            Tasks.title,
+            Tasks.description,
+            Status.name.label('status')
+        ).join(Tasks, Users.id == Tasks.user_id
+               ).join(Status, Tasks.status_id == Status.id
+                      ).filter(Users.email.like(f"%{pattern}%")
+                               ).all()
+
+        return result
+
+    def get_number_of_tasks_for_each_status(self):
+        return self.session.query(Status.name, func.count(Tasks.id)).join(Tasks).group_by(Status.name).all()
+
+    def get_random_task(self):
+        tasks = self.get_all_tasks()
+        return choice(tasks)
+
     def delete_all_tasks(self):
         self.session.query(Tasks).delete()
+        self.session.commit()
+
+    def delete_task(self, task_id: int):
+        task = self.session.query(Tasks).filter(Tasks.id == task_id).first()
+        self.session.delete(task)
         self.session.commit()
 
     def setup(self, users_number: int = 10, status_list: list = None, task_number: int = 10):
